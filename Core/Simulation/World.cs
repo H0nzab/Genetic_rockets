761915d9d2e897101b4a55d0c61a356755d829eb
bartoja4 <37354987@cuni.cz>
2026-05-20 15:01:42 +0200

    Created structure

---
+++ Core/Simulation/World.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genetic_rockets.Core.Simulation
+{
+    class World
+    {
+    }
+}
---
+++ UI/Rendering/SimulationRenderer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genetic_rockets.UI.Rendering
+{
+    class SimulationRenderer
+    {
+    }
+}
