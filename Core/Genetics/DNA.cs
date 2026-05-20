df433cdaf92788b67b175f21bb90fa66f75b73c1
bartoja4 <37354987@cuni.cz>
2026-05-20 15:02:46 +0200

    class DNA

--- Core/Genetics/DNA.cs
+++ Core/Genetics/DNA.cs
@@ -1,10 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 
 namespace Genetic_rockets.Core.Genetics
 {
     class DNA
     {
+        // Samotné geny
+        // Vektor je vestavěný
+        public Vector[] Genes { get; private set; }
+
+        private static readonly Random _random = new Random();
+
+        private readonly double _maxForce;
+
+        public DNA(int lifespan, double maxForce)
+        {
+            _maxForce = maxForce;
+            Genes = new Vector[lifespan];
+
+            for (int i = 0; i < lifespan; i++)
+            {
+                Genes[i] = GenerateRandomVector();
+            }
+        }
+
+        private DNA(Vector[] newGenes, double maxForce)
+        {
+            Genes = newGenes;
+            _maxForce = maxForce;
+        }
+
+        private Vector GenerateRandomVector()
+        {
+            double angle = _random.NextDouble() * Math.PI * 2; // Úhel v radiánech
+            return new Vector(Math.Cos(angle) * _maxForce, Math.Sin(angle) * _maxForce);
+        }
+
+        // Křížení DNA
+        public DNA Crossover(DNA partner)
+        {
+            Vector[] childGenes = new Vector[Genes.Length];
+
+            // Náhodně zvolíme bod zlomu
+            int midpoint = _random.Next(Genes.Length);
+
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                // Polovina genů od jednoho rodiče, polovina od druhého
+                if (i > midpoint)
+                {
+                    childGenes[i] = Genes[i];
+                }
+                else
+                {
+                    childGenes[i] = partner.Genes[i];
+                }
+            }
+
+            return new DNA(childGenes, _maxForce);
+        }
+
+        public void Mutate(double mutationRate)
+        {
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                if (_random.NextDouble() < mutationRate)
+                {
+                    Genes[i] = GenerateRandomVector();
+                }
+            }
+        }
     }
 }
