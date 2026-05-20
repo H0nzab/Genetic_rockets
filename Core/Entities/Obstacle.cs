7b3d4f8a0105fa06ce75d3e978e025d930b8336a
bartoja4 <37354987@cuni.cz>
2026-05-20 15:18:08 +0200

    classes Obstacle and World

--- Core/Entities/Obstacle.cs
+++ Core/Entities/Obstacle.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows;
 
 namespace Genetic_rockets.Core.Entities
 {
-    class Obstacle
+    public class Obstacle
     {
+        public Rect Bounds { get; private set; }
+
+        public Obstacle(double x, double y, double width, double height)
+        {
+            Bounds = new Rect(x, y, width, height);
+        }
+
+        public bool Contains(Point point)
+        {
+            return Bounds.Contains(point);
+        }
     }
 }
--- Core/Simulation/World.cs
+++ Core/Simulation/World.cs
@@ -1,10 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Windows;
+using Genetic_rockets.Core.Entities;
 
 namespace Genetic_rockets.Core.Simulation
 {
-    class World
+    public class World
     {
+        public Point Target { get; private set; }
+        public List<Obstacle> Obstacles { get; private set; }
+
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+
+        public World(double width, double height, Point target)
+        {
+            Width = width;
+            Height = height;
+            Target = target;
+            Obstacles = new List<Obstacle>();
+        }
+
+        public void AddObstacle(double x, double y, double width, double height)
+        {
+            Obstacles.Add(new Obstacle(x, y, width, height));
+        }
+
+        // Kontrola kolize rakety
+        public bool IsCollision(Point position)
+        {
+            if (position.X < 0 || position.X > Width || position.Y < 0 || position.Y > Height)
+            {
+                return true;
+            }
+
+            foreach (var obstacle in Obstacles)
+            {
+                if (obstacle.Contains(position))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsAtTarget(Point position, double targetRadius = 10)
+        {
+            return (Target - position).Length <= targetRadius;
+        }
     }
 }
